124714efbd5d6dea90ac265664c981ae7c7ca9f6
tom <tom@ceacsz.com>
2019-02-13 11:32:55 +0800

    ignore some files

---
+++ TestOpenSSL/TestOpenSSL/test.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <openssl/md5.h> // md5 头文件
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void getMD5(const char* str, char* result)
+{
+	MD5_CTX ctx;
+	// 初始化
+	MD5_Init(&ctx);
+	// 添加数据
+	MD5_Update(&ctx, str, strlen(str));
+	// 计算结果
+	unsigned char md[16] = { 0 };
+	MD5_Final(md, &ctx);
+	for (int i = 0; i < 16; ++i)
+	{
+		sprintf(&result[i * 2], "%02x", md[i]);
+	}
+}
+int main()
+{
+	char result[33] = { 0 };
+	getMD5("hello, md5", result);
+	printf("md5 value: %s\n", result);
+	system("pause");
+	return 0;
+}
